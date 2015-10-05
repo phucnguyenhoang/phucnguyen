16512868cbf0d8ca30d4d55485022fb1d44beee3
unknown <nhphuc@isb-482.isb-vietnam.com>
2015-10-05 11:36:48 +0700

    add loading animation

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Test animation</title>
+    <title>Phuc's home</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="mobile-web-app-capable" content="yes">
@@ -12,6 +12,67 @@
 
 </head>
 <body class="layout fullbleed">
-    <main-container></main-container>
+	<dom-module id="main-view">
+		<style is="custom-style">
+			#loading {
+				@apply(--layout-fit);
+				@apply(--layout-horizontal);
+				@apply(--layout-center-justified);
+				background-color: var(--paper-grey-50);
+			}
+			#loading paper-spinner {
+				@apply(--layout-self-center);
+				display: block;
+			}
+		</style>
+		<template>
+			<main-container></main-container>
+			<div id="loading">
+				<paper-spinner active></paper-spinner>
+			</div>			
+		</template>
+		<script>
+			Polymer({
+				is: 'main-view',
+				behaviors: [
+					Polymer.NeonAnimationRunnerBehavior
+				],
+				properties: {
+					animationConfig: {
+						value: function() {
+							return {
+								'exit': [{
+									name: 'scale-down-animation',
+									node: this.$.loading,
+									axis: '',
+									timing: {
+										duration: 1000
+									}
+								}, {
+									name: 'fade-out-animation',
+									node: this.$.loading,
+									timing: {
+										delay: 50,
+										duration: 800
+									}
+								}]
+							}
+						}
+					}
+				},
+				listeners: {
+					'neon-animation-finish': '_onLoad'
+				},
+				ready: function() {
+					this.playAnimation('exit');
+					console.log('ready');
+				},
+				_onLoad: function() {
+					this.$.loading.style.display = 'none';
+				}
+			});
+		</script>
+	</dom-module>
+	<main-view></main-view>
 </body>
 </html>
