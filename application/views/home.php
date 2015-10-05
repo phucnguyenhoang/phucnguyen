6f8212110199021118310bc9186516dc798ad145
phucnguyen <hoangphuc.ag@gmail.com>
2015-10-05 09:50:08 +0700

    initial project

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Nguyen Hoang Phuc</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <script type="text/javascript" src="<?php echo base_url('resources/bower_components/webcomponentsjs/webcomponents.min.js'); ?>"></script>
+
+    <link rel="import" href="../../resources/elements/home-view.html">
+    <link rel="import" href="../../resources/custom-style.html">
+</head>
+<body class="fullbleed layout vertical">
+    <home-view>content here</home-view>
+</body>
+</html>
---
+++ application/views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Test animation</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <script type="text/javascript" src="<?php echo base_url('resources/bower_components/webcomponentsjs/webcomponents.min.js'); ?>"></script>
+	<link rel="import" href="../../resources/elements/elements.html">
+
+</head>
+<body class="layout fullbleed">
+    <main-container></main-container>
+</body>
+</html>
